872a62027f49f23c64c3c68fd06b9314485d9b19
Jeff Olivier <jeff@jeffolivier.com>
2025-01-09 11:18:48 -0500

    Filter games by platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,12 @@
 import useData from "./useData";
+import { Platform } from "./useGames";
 
-export interface Platforms {
-    id: number;
-    name: string;
-    slug: string;
-}
+// export interface Platforms {
+//     id: number;
+//     name: string;
+//     slug: string;
+// }
 
-const usePlatforms = () => useData<Platforms>("/platforms/lists/parents");
+const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
 export default usePlatforms;
