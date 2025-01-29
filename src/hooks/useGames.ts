cbe264f1f14effe82c40a6f2c73142ecdb8d4eee
Jeff Olivier <jeff@jeffolivier.com>
2025-01-29 16:16:10 -0500

    Implements Infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
-// import gameService, { Game } from "../services/gameService";
 import { Platform } from "./usePlatforms";
+
+const PAGE_SIZE = 10;
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -16,17 +17,22 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) => {
-    return useQuery<FetchResponse<Game>, Error>({
+    return useInfiniteQuery<FetchResponse<Game>, Error>({
         queryKey: ["games", gameQuery],
-        queryFn: () =>
+        queryFn: ({ pageParam = 1 }) =>
             apiClient.getAll({
                 params: {
                     genres: gameQuery.genre?.id,
                     parent_platforms: gameQuery.platform?.id,
                     ordering: gameQuery.sortOrder,
                     search: gameQuery.searchText,
+                    page_size: PAGE_SIZE,
+                    page: pageParam /*gameQuery.page ? gameQuery.page : 1,*/,
                 },
             }),
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.next ? allPages.length + 1 : undefined;
+        },
     });
 };
 
