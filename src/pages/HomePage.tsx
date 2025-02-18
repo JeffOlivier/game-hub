7030850a07ccb5551fe3961effb5feba52f15f3e
Jeff Olivier <jeff@jeffolivier.com>
2025-02-18 15:04:56 -0500

    Setup Routing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,42 @@
+import { Grid, GridItem, Show, Box, Flex } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import NavBar from "../components/NavBar";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+
+const HomePage = () => {
+    return (
+        <Grid
+            templateAreas={{
+                base: `"main"`,
+                lg: `"aside main"`, // wider than 1024px
+            }}
+            templateColumns={{
+                base: "1fr",
+                lg: "200px 1fr",
+            }}
+        >
+            <Show above="lg">
+                <GridItem area="aside" paddingX={5}>
+                    <GenreList />
+                </GridItem>
+            </Show>
+            <GridItem area="main">
+                <Box paddingLeft={2} mb={2}>
+                    <GameHeading />
+                    <Flex>
+                        <Box marginRight={5}>
+                            <PlatformSelector />
+                        </Box>
+                        <SortSelector />
+                    </Flex>
+                </Box>
+                <GameGrid />
+            </GridItem>
+        </Grid>
+    );
+};
+
+export default HomePage;
