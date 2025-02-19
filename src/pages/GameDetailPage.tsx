9ec50a493389a7d7ef36c199aa6fc20484973004
Jeff Olivier <jeff@jeffolivier.com>
2025-02-19 16:40:35 -0500

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,10 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useGameDetails from "../hooks/useGameDetails";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
     const params = useParams();
@@ -21,6 +22,7 @@ const GameDetailPage = () => {
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game} />
             <GameTrailer gameId={game.id} />
+            <GameScreenshots gameId={game.id} />
         </>
     );
 };
