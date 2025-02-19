5afc7116ad93eab85762ecf71b123c9e57410f2c
Jeff Olivier <jeff@jeffolivier.com>
2025-02-19 15:57:26 -0500

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,24 +1,13 @@
 import { Link, useParams } from "react-router-dom";
 import useGameDetails from "../hooks/useGameDetails";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
     const params = useParams();
     // const {slug} = useParams();
-
-    // const {
-    //     data,
-    //     error,
-    //     isLoading,
-    //     // isFetchingNextPage,
-    //     fetchNextPage,
-    //     hasNextPage,
-    //     // } = useGames(gameQuery);
-    // } = useGameDetails(params.id);
 
     const { data: game, isLoading, error } = useGameDetails(params.slug!);
 
@@ -31,9 +20,7 @@ const GameDetailPage = () => {
             <Heading>{game.name}</Heading>
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game} />
-            <br />
-            <br />
-            <Link to="/">HOME</Link>
+            <GameTrailer gameId={game.id} />
         </>
     );
 };
